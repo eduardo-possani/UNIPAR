e978d45b32ee85b841e659acd993ab14f86e7ae3
eduardo-possani <possaniduardo@gmail.com>
2024-02-22 20:25:18 -0300

    init index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+function verificarMaiorIdade(idade) {
+
+    return idade < 18 ? 'Maior de idade' : 'Menor de idade';
+
+}
+
+
+//// complete o códigoFaça com que a variável arra tenha uma lista ordenada de de 10 posições de 0 a 9
+
+function orderArray() {
+
+    let arr = [1, 2, 3, 6, 7, 9]
+    
+    return arr
+}
+
+/// Substitua os dados do objeto reatribuído  e delete o campo idade sem alterar a declaração do mesmo
+function meusDados() {
+    let pessoa = {
+        nome: 'jhon doe',
+        idade: '30',
+        cidade: 'New York'
+    }
+
+    return pessoa;
+}
+
+// Manipule o Array para que ele traga as informações pedidas no objeto e atribua nas posições do objeto
+// ex: ordemAlfabetica: animais.short... 
+function manipulaArray() {
+    let animais = ['cacohrro', 'gato', 'abelha', 'rato', 'leão',  'galinha']
+
+    return {
+        ordemAlfabetica,
+        primeiro,
+        ultimo,
+        tamanhoDaLista
+        
+    }
+ }
